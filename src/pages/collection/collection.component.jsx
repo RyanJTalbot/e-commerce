3a1fdf89cb181b537c9fa4baad7438379cf02e51
Ryan JT <ryantalbot7@gmail.com>
2020-07-22 16:47:27 -0500

    Add Collection routing and memoizing selectCollection

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selector';
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionid)
+const CollectionPage = ({ collection }) => {
+  console.log(collection)
   return (
     <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
@@ -11,4 +13,8 @@ const CollectionPage = ({ match }) => {
   )
 }
 
-export default CollectionPage; 
+ const mapStateToProps = (state, ownProps) => ({
+   collection: selectCollection(ownProps.match.params.collectionId)(state)
+ })
+
+export default connect(mapStateToProps)(CollectionPage); 
